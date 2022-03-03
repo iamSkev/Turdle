b27868e22203dfe5a2e505f9acae03f3f1f7270d
skev <shawnkenzov@gmail.com>
2022-03-03 11:27:36 +0800

    Added say command

---
+++ cogs/utils.py
@@ -0,0 +1,18 @@
+import disnake
+from disnake.ext import commands
+
+ALLOWED_ROLES = 884136022592610334
+
+class Utilities(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+    
+    @commands.command
+    @commands.has_any_role(ALLOWED_ROLES)
+    async def say(self, ctx, channe=None,* , phrase):
+        channel = channel or ctx.channel
+        await channel.send(phrase)
+    
+
+def setup(bot):
+    bot.add_cog(Utilities(bot))
---
+++ main.py
@@ -0,0 +1,35 @@
+import asyncio
+import disnake
+from dotenv import load_dotenv
+import os
+from disnake.ext import commands
+
+bot = commands.Bot(prefix="t!")
+
+load_dotenv()
+token = os.getenv("BOT_TOKEN")
+
+@bot.command()
+@commands.is_owner()
+async def load(ctx, extension):
+  bot.load_extension(f'cogs.{extension}')
+  await ctx.send("Succesfully did it poggers")
+
+@bot.command()
+@commands.is_owner()
+async def unload(ctx, extension):
+  bot.unload_extension(f'cogs.{extension}')
+  await ctx.send("Succesfully did it poggers")
+
+@bot.command()
+@commands.is_owner()
+async def reload(ctx, extension):
+  bot.unload_extension(f'cogs.{extension}')
+  bot.load_extension(f'cogs.{extension}')
+  await ctx.send("Did it")
+
+for filename in os.listdir("./cogs"):
+  if filename.endswith(".py"):
+    bot.load_extension(f"cogs.{filename[:-3]}")
+
+bot.run()
