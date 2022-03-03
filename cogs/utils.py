827504d9eb843c3ff814b8663e74ceaf26bd617b
skev <shawnkenzov@gmail.com>
2022-03-03 12:50:59 +0800

    Added say command

--- cogs/utils.py
+++ cogs/utils.py
@@ -1,4 +1,5 @@
 import disnake
+from typing import Optional
 from disnake.ext import commands
 
 ALLOWED_ROLES = 884136022592610334
@@ -9,17 +10,13 @@ class Utilities(commands.Cog):
     
     @commands.command()
     @commands.has_any_role(ALLOWED_ROLES)
-    async def say(self, ctx, channel: disnake.TextChannel,* , phrase):
+    async def say(self, ctx, channel: Optional[disnake.TextChannel],* , phrase):
         await channel.send(phrase)
     
     @say.error
     async def say_error(self, ctx, error):
         if isinstance(error, commands.MissingRequiredArgument):
-            param = error.param
-            if str(param) == "channel: disnake.channel.TextChannel":
-                await ctx.send("Sorry bub no can do. I need a channel to send stuff to.")
-            else:
-                await ctx.send("Hey! I actually need something to send.") 
+            await ctx.send("Sorry bub no can do. I actually need something to send.") 
 
 def setup(bot):
     bot.add_cog(Utilities(bot))
