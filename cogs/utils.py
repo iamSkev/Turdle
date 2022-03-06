c7e63489f25651216b9454653218ba2daf202026
skev <shawnkenzov@gmail.com>
2022-03-06 14:33:34 +0800

    Added userinfo command

--- cogs/utils.py
+++ cogs/utils.py
@@ -27,6 +27,28 @@ class Utilities(commands.Cog):
         channel = channel or ctx.channel
         await channel.send(phrase)
     
+    @commands.command()
+    async def userinfo(self, ctx, user: int = None) -> None:
+        user = self.bot.get_user(user)
+        if user is None:
+            await ctx.send("Could not get info about that userid. Perhaps its wrong or it does not exist")
+            return
+        date_format = "%a, %d %b %Y %I:%M %p"
+        embed = disnake.Embed(color=0xdfa3ff, description=user.mention)
+        embed.set_author(name=str(user), icon_url=user.display_avatar)
+        embed.set_thumbnail(url=user.display_avatar)
+        embed.add_field(name="Joined", value=user.joined_at.strftime(date_format))
+        members = sorted(ctx.guild.members, key=lambda m: m.joined_at)
+        embed.add_field(name="Join position", value=str(members.index(user)+1))
+        embed.add_field(name="Registered", value=user.created_at.strftime(date_format))
+        if len(user.roles) > 1:
+            role_string = ' '.join([r.mention for r in user.roles][1:])
+            embed.add_field(name="Roles [{}]".format(len(user.roles)-1), value=role_string, inline=False)
+        perm_string = ', '.join([str(p[0]).replace("_", " ").title() for p in user.guild_permissions if p[1]])
+        embed.add_field(name="Guild permissions", value=perm_string, inline=False)
+        embed.set_footer(text='ID: ' + str(user.id))
+        await ctx.send(embed=embed)
+
     @say.error
     async def say_error(self, ctx, error) -> None:
         if isinstance(error, commands.MissingRequiredArgument):
