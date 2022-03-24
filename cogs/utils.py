0c760051981e552cb67bc553e936a254331c51b4
skev <shawnkenzov@gmail.com>
2022-03-24 09:59:04 +0800

    member join

--- cogs/utils.py
+++ cogs/utils.py
@@ -37,7 +37,7 @@ class Utilities(commands.Cog):
         embed = disnake.Embed(title="Welcome to Pomelo SMP!", description="You should go and check out <#955306783130386442> & the <#955306855305990174>!", colour=0xFFE684, timestamp=datetime.utcnow())
         embed.set_thumbnail(url="https://media.discordapp.net/attachments/956026532105707560/956033570558861342/axolotl-minecraft.gif")
         embed.set_image(url="https://media.discordapp.net/attachments/956026532105707560/956034300518752276/minecraft-axolotl.gif")
-        await channel.send(f"{member.mention} has just arrived! Say hi and slap them on the ass!")
+        await channel.send(f"{member.mention} has just arrived! Say hi and slap them on the ass!", embed=embed)
 
     @commands.command()
     async def test(self, ctx):
